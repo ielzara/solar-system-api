b9af61232f75e597967ffe96c40020fc76d56a63
somy <smycho28@gmail.com>
2024-10-25 11:40:00 -0700

    create model

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,13 +1,22 @@
-class Planet:
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
 
-    def __init__(self, id, name, description, has_moon):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.has_moon = has_moon
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    has_moon: Mapped[bool]
 
-planets = [
-    Planet(1, "Mars", "4217 miles in diameter, very cold and red", True),
-    Planet(2, "Earth", "7926 miles in diameter, habitable and blue", True),
-    Planet(3, "Venus", "3760.4 miles in diameter, hottest planet and red", False)
-    ]
+# class Planet:
+
+#     def __init__(self, id, name, description, has_moon):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.has_moon = has_moon
+
+# planets = [
+#     Planet(1, "Mars", "4217 miles in diameter, very cold and red", True),
+#     Planet(2, "Earth", "7926 miles in diameter, habitable and blue", True),
+#     Planet(3, "Venus", "3760.4 miles in diameter, hottest planet and red", False)
+#     ]
--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -1,46 +1,46 @@
 from flask import Blueprint, abort, make_response
-from app.models.planet import planets
+from app.models.planet import Planet
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
 
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except ValueError:
-        response = {"message": f"planet {planet_id} invalid"}
-        abort(make_response(response, 400))
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except ValueError:
+#         response = {"message": f"planet {planet_id} invalid"}
+#         abort(make_response(response, 400))
 
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
 
-    response = {"message": f"planet {planet_id} not found"}
-    abort(make_response(response, 404))
-
-
-@planets_bp.get("")
-def get_all_planets():
-    planets_response = []
-    for planet in planets:
-        planets_response.append(
-            {
-                "id":planet.id,
-                "name":planet.name,
-                "description":planet.description,
-                "has_moon":planet.has_moon,
-            }
-        )
-    return planets_response
+#     response = {"message": f"planet {planet_id} not found"}
+#     abort(make_response(response, 404))
 
 
-@planets_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
+# @planets_bp.get("")
+# def get_all_planets():
+#     planets_response = []
+#     for planet in planets:
+#         planets_response.append(
+#             {
+#                 "id":planet.id,
+#                 "name":planet.name,
+#                 "description":planet.description,
+#                 "has_moon":planet.has_moon,
+#             }
+#         )
+#     return planets_response
+
+
+# @planets_bp.get("/<planet_id>")
+# def get_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
     
-    return {
-        "id":planet.id,
-        "title":planet.name,
-        "description":planet.description,
-        "has_moon":planet.has_moon,
-    }
+#     return {
+#         "id":planet.id,
+#         "title":planet.name,
+#         "description":planet.description,
+#         "has_moon":planet.has_moon,
+#     }
