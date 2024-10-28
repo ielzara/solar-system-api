9f1d29244f3aeae680b58f2a23cea86e00a1c2aa
somy <smycho28@gmail.com>
2024-10-27 18:41:22 -0700

    Define a route to read model records by accessing from back-end layer, part 3

--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -4,6 +4,23 @@ from ..db import db
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
+@planets_bp.get("")
+def get_all_planets():
+    query=db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+
+    planets_response=[]
+    for planet in planets:
+        planets_response.append(
+            {
+                "id":planet.id,
+                "name":planet.name,
+                "description":planet.description,
+                "has_moon":planet.has_moon
+            }
+        )
+    return planets_response
+
 @planets_bp.post("")
 def create_planet():
     request_body = request.get_json()
