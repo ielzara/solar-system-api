ae1c743664b7d7e2a5b46ddcf3949d03beddf466
Elzara Turakulova <endjik@gmail.com>
2024-10-29 11:55:56 -0700

    added get one planet, update and delete functions

--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.planet import Planet
 from ..db import db 
 
@@ -40,6 +40,54 @@ def create_planet():
     }
     return response, 201
 
+@planets_bp.get('/<planet_id>')
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "has_moon": planet.has_moon,
+    }
+
+
+@planets_bp.put("/<planet_id>")
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.has_moon = request_body["has_moon"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+@planets_bp.delete("<planet_id>")
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+    db.session.delete(planet)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        response = {"message": f"planet {planet_id} invalid"}
+        abort(make_response(response, 400))
+
+    query = db.select(Planet).where(Planet.id == planet_id)
+    planet = db.session.scalar(query)
+
+    if not planet:
+        response = {"message": f"planet {planet_id} not found"}
+        abort(make_response(response, 404))
+
+    return planet
+
 # def validate_planet(planet_id):
 #     try:
 #         planet_id = int(planet_id)
@@ -73,7 +121,7 @@ def create_planet():
 # @planets_bp.get("/<planet_id>")
 # def get_one_planet(planet_id):
 #     planet = validate_planet(planet_id)
-    
+
 #     return {
 #         "id":planet.id,
 #         "title":planet.name,
